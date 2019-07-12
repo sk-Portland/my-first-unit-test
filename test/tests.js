bc0d5498a5442bcc8dfa54be02128edb32dd61f1
Susan K <kralsusa@yahoo.com>
2019-07-11 19:05:07 -0700

    more  progress

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,16 @@
+import add from ".."/add.js;
 const test = QUnit.test;
 
-test('time to test!', function(assert) {
+test('adds two numbers', function (assert))
     //Arrange
     // Set up your parameters and expectations
+    const x = 3;
+    const y = 4;
+    cons expected = 7;
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const sum = add(x, y);
     //Assert
     // What should the result be?
     assert.equal(true, false);
